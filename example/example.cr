e266a9458a24b22a19e9951b329b6b98a15cd96d
apainintheneck <apainintheneck@gmail.com>
2024-01-06 23:41:01 -0800

    Add help and docs for example commands

--- example/example.cr
+++ example/example.cr
@@ -2,15 +2,28 @@ require "option_parser"
 
 require "../src/linenoise"
 
+HELP_TEXT = <<-HELP
+[REPL Commands]
+  :historylen <length> -- Change the history length.
+  :singleline          -- Enter single line editing mode.
+  :multiline           -- Enter multiline editing mode.
+  :mask                -- Enter masked editing mode.
+  :unmask              -- Exit masked editing mode.
+  :clear               -- Clear screen.
+  :help                -- Show this message
+  :debug               -- Print key codes.
+  :exit                -- Exit the program.
+  :quit                -- Quit the program.
+HELP
+
 async = false
 
 OptionParser.parse do |parser|
-  parser.banner = "Usage: ./example [option...]"
-  parser.on("--keycodes", "Print out keycodes for debugging") do
-    Linenoise.print_key_codes
-  end
+  parser.banner = "Usage: crystal run example/example.cr"
   parser.on("-h", "--help", "Show this help") do
     puts parser
+    puts
+    puts HELP_TEXT
     exit
   end
   parser.invalid_option do |flag|
@@ -22,8 +35,7 @@ end
 
 HISTORY_FILE = "#{__DIR__}/example_history.txt"
 
-Linenoise.add_completions(["hello", "hello there"], with_hints: true)
-# Linenoise.add_hints({ "hello there" => Linenoise::Hint.new(text: " cruel world!", color: Colorize::ColorANSI::Red)})
+Linenoise.add_completions(["hi", "hello", "hello there"], with_hints: true)
 Linenoise.load_history(HISTORY_FILE)
 
 loop do
@@ -42,6 +54,7 @@ loop do
       puts "Error: Invalid length: #{args[2]?}"
     else
       Linenoise.max_history(len)
+      Linenoise.load_history(HISTORY_FILE)
     end
   when ":singleline"
     Linenoise.set_multiline(false)
@@ -53,7 +66,12 @@ loop do
     Linenoise.set_mask_mode(false)
   when ":clear"
     Linenoise.clear_screen
+  when ":help"
+    puts HELP_TEXT
+  when ":debug"
+    Linenoise.print_key_codes
   when ":exit", ":quit"
+    puts "Have a nice day!"
     break
   else
     puts "echo: #{line}"
